2ccdf6a49dc4bd9b92eb3f300f89589b022a8753
Jahna Arielle Goldman <jagoldman@cs.uchicago.edu>
2016-08-15 23:02:23 -0500

    init commit

---
+++ src/test/java/edu/uchicago/jagoldman/SportsVansApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.uchicago.jagoldman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportsVansApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
