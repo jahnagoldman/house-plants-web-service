9a7d8ab64e1c3ce056dd23a1fdc8897982778195
Jahna Arielle Goldman <jagoldman@cs.uchicago.edu>
2016-08-16 16:48:53 -0500

    created entities

--- src/main/java/edu/uchicago/jagoldman/domain/Make.java
+++ src/main/java/edu/uchicago/jagoldman/domain/Make.java
@@ -1,7 +1,38 @@
 package edu.uchicago.jagoldman.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Created by jahnaariellegoldman on 8/15/16.
  */
+@Entity
 public class Make {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Make(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/edu/uchicago/jagoldman/domain/Van.java
+++ src/main/java/edu/uchicago/jagoldman/domain/Van.java
@@ -1,7 +1,98 @@
 package edu.uchicago.jagoldman.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by jahnaariellegoldman on 8/15/16.
  */
+@Entity
 public class Van {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String desc;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    @ManyToOne
+    private Make make;
+
+    private String imgurl;
+
+    public Van(String name, String desc, Make make, String imgurl) {
+        this.name = name;
+        this.desc = desc;
+        this.make = make;
+        this.imgurl = imgurl;
+        createTime = new Date();
+    }
+
+    public Van() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public Make getMake() {
+        return make;
+    }
+
+    public void setMake(Make make) {
+        this.make = make;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    @Override
+    public String toString() {
+        return "Van{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", createTime=" + createTime +
+                ", make=" + make +
+                ", imgurl='" + imgurl + '\'' +
+                '}';
+    }
 }
